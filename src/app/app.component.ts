f4d81c1ef30eb5ba348c7c47be4f8172a0a13a33
kn-nalbantov <kn.nalbantov@gmail.com>
2022-03-21 19:27:14 +0200

    added sample add component content and style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mdn-angular-todo';
+  title = 'todo app';
+  purpose = 'Following mdn excercise to learn Angular';
 }
