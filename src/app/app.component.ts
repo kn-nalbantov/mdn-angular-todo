e0154d9136daa81549789fc030bee4717dff0cbd
kn-nalbantov <kn.nalbantov@gmail.com>
2022-03-22 19:24:32 +0200

    added item adding logic and template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo app';
-  purpose = 'Following mdn excercise to learn Angular';
+  title = 'mdn-angular-todo';
+
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems = [
+    { description: 'eat', done: true },
+    { description: 'sleep', done: false },
+    { description: 'learn', done: false },
+    { description: 'play', done: false },
+  ];
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) =>
+      this.filter === 'done' ? item.done : !item.done
+    );
+  }
+
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      done: false,
+    });
+  }
 }
